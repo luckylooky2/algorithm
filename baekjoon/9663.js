6c5520cb2f381166aa4adce7f8dfb2fe4f5d7b5a
Chanhyung Lee <luckylooky2@naver.com>
2023-07-21 18:33:33 +0900

    07.21 : backtracking(nqueen)

---
+++ baekjoon/9663.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const input = parseInt(fs.readFileSync("/dev/stdin").toString().trim(), 10);
+
+let count = 0;
+
+function cannotPlace(result, currentRow) {
+  let offset;
+  for (let i = 1; i < currentRow; i++) {
+    offset = currentRow - i;
+    // 같은 열 검사
+    if (result[i] === result[currentRow]) return true;
+    // 대각선 검사
+    if (
+      result[i] + offset === result[currentRow] ||
+      result[i] - offset === result[currentRow]
+    )
+      return true;
+  }
+  return false;
+}
+
+function nQueen(n, r = 0, result = [], depth = 0) {
+  result[depth] = r;
+  // 유망성 검사
+  if (depth > 1 && cannotPlace(result, depth)) {
+    return;
+  }
+  // 해 검사
+  if (n === depth) {
+    count++;
+    return;
+  }
+
+  for (let i = 1; i <= input; i++) {
+    nQueen(n, i, result, depth + 1);
+  }
+}
+
+nQueen(input);
+
+console.log(count);
