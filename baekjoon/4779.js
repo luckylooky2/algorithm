1ad009b0892e94ca85d30f66a928b00b418cf91f
Chanhyung Lee <luckylooky2@naver.com>
2023-08-03 21:37:07 +0900

    08.03 : divide conquer(cantorian set)

---
+++ baekjoon/4779.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => parseInt(v, 10));
+
+const final = [];
+
+function recur(n, string, i = 0) {
+  if (i === 1) return string + " ".repeat(n);
+  if (n === 1) return string + "-";
+
+  for (let i = 0; i < 3; i++) {
+    string = recur(n / 3, string, i);
+  }
+  return string;
+}
+
+input.forEach((element) => {
+  let result = "";
+  const n = Math.pow(3, element);
+  result = recur(n, result);
+  final.push(result);
+});
+
+console.log(final.join("\n"));
