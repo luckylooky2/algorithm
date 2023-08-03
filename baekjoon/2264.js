99bf8215cb822d1a90c7ba28c6978b3e43513a3e
luckylooky2 <luckylooky2@naver.com>
2023-08-03 10:03:16 +0900

    08.02 : using index Object to set O(1)

--- baekjoon/2264.js
+++ baekjoon/2264.js
@@ -7,25 +7,17 @@ const [length, inOrder, postOrder] = fs
   .map((v) => v.split(" ").map((v) => parseInt(v, 10)));
 
 const n = inOrder.length;
+const index = {};
 const final = [];
 
+inOrder.map((v, i) => (index[v] = i));
+
 function recur(n, inStart, inEnd, postStart, postEnd) {
-  if (n === 1) {
-    // inOrder? postOrder?
-    final.push(inOrder[inStart]);
-    // console.log("push :", inOrder[inStart]);
-    return;
-  }
-  if (n == 2) {
-    final.push(postOrder[postEnd]);
-    final.push(postOrder[postStart]);
-    // console.log("push :", postOrder[postEnd], postOrder[postStart]);
-    return;
-  }
+  if (n === 1) return final.push(inOrder[inStart]);
+  if (n === 2) return final.push(postOrder[postEnd], postOrder[postStart]);
 
   const root = postOrder[postEnd];
-  // right 노드에서는 조정이 필요
-  const rootIndex = inOrder.findIndex((elem) => elem === root);
+  const rootIndex = parseInt(index[root], 10);
 
   const firstStart = inStart;
   const firstEnd = rootIndex - 1;
@@ -33,24 +25,17 @@ function recur(n, inStart, inEnd, postStart, postEnd) {
   const secondStart = rootIndex + 1;
   const secondEnd = inEnd;
   const second = secondEnd - secondStart + 1;
-  // 맨 뒤는 빼고
   const firstPostStart = postStart;
   const firstPostEnd = postStart + first - 1;
   const secondPostStart = postEnd - 1 - second + 1;
   const secondPostEnd = postEnd - 1;
-  // console.log(inStart, inEnd, n);
-  // console.log("root index : ", root, rootIndex);
-  // console.log("index : ", firstStart, firstEnd, firstPostStart, firstPostEnd);
-  // console.log("count : ", first, second);
-  // console.log("post : ", postStart, postEnd, secondPostStart, secondPostEnd);
 
   // root 노드 추가
-  // console.log("push :", inOrder[rootIndex]);
   final.push(inOrder[rootIndex]);
-  // left 노드
+  // left-side 재귀 호출
   if (firstEnd >= firstStart)
     recur(first, firstStart, firstEnd, firstPostStart, firstPostEnd);
-  // right 노드
+  // right-side 재귀 호출
   if (secondEnd >= secondStart)
     recur(second, secondStart, secondEnd, secondPostStart, secondPostEnd);
 }
