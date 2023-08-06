cae74a2c8a1c01bc93f221c88f77e8b2d4e2766a
luckylooky2 <luckylooky2@naver.com>
2023-08-06 23:58:54 +0900

    08.06 : fix range sum segment tree(using tree, start / end index)

--- baekjoon/segment2.js
+++ baekjoon/segment2.js
@@ -11,14 +11,19 @@ function segtree(start, end, node = 1) {
 
 function query(start, end, currLeft, currRight, node = 1) {
   if (currRight < start || currLeft > end) return Infinity;
-  if (start <= currLeft && currRight <= end) {
-    let res = 0;
-    for (let i = currLeft; i <= currRight; i++) res += arr[i];
-    return res;
-  }
-  const mid = Math.floor((start + end) / 2);
-  const leftValue = query(start, end, start, mid, 2 * node);
-  const rightValue = query(start, end, mid + 1, end, 2 * node + 1);
+  // 1. 트리를 이용
+  if (start <= currLeft && currRight <= end) return tree[node];
+  // if (start <= currLeft && currRight <= end) {
+  //   let res = 0;
+  //   for (let i = currLeft; i <= currRight; i++) res += arr[i];
+  //   return res;
+  // }
+  const mid = Math.floor((currLeft + currRight) / 2);
+  // 2. 시작과 끝은 currLeft, currRight => start, end가 아님
+  const leftValue = query(start, end, currLeft, mid, 2 * node);
+  const rightValue = query(start, end, mid + 1, currRight, 2 * node + 1);
+  // const leftValue = query(start, end, start, mid, 2 * node);
+  // const rightValue = query(start, end, mid + 1, end, 2 * node + 1);
   if (leftValue === Infinity) return rightValue;
   else if (rightValue === Infinity) return leftValue;
   else return leftValue + rightValue;
