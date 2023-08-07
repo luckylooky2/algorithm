b81ce082acfa59ff24545a914bf530be98c1143d
Chanhyung Lee <luckylooky2@naver.com>
2023-08-07 22:01:52 +0900

    08.07 : dynamic programming(fibonacci)

---
+++ baekjoon/1003.js
@@ -0,0 +1,18 @@
+const arr = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => parseInt(v, 10));
+
+arr.shift();
+const cache = { 0: 0, 1: 1 };
+let i = 2;
+
+arr.forEach((element) => {
+  while (i <= element) {
+    cache[i] = cache[i - 1] + cache[i - 2];
+    i++;
+  }
+  console.log(element ? cache[element - 1] : 1, cache[element]);
+});
