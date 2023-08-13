8aa33e6c57671d260cc5b68ed259e61bc8dfde7f
luckylooky2 <luckylooky2@naver.com>
2023-08-13 10:24:04 +0900

    08.12 : dynamic programming(2xn tile)

---
+++ baekjoon/11726.js
@@ -0,0 +1,12 @@
+const n = parseInt(
+  require("fs").readFileSync("/dev/stdin").toString().trim(),
+  10
+);
+
+const dp = new Array(n + 1).fill(1);
+
+for (let i = 2; i <= n; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+console.log(dp[n]);
