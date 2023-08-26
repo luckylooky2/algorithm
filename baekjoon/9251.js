54d568485f032dbdd838b6f8392617b0497240e6
luckylooky2 <luckylooky2@naver.com>
2023-08-26 21:15:03 +0900

    08.26 : dp(LCS)

--- baekjoon/9251.js
+++ baekjoon/9251.js
@@ -10,7 +10,7 @@ const dp = new Array(second.length + 1)
 
 for (let i = 1; i <= second.length; i++) {
   for (let j = 1; j <= first.length; j++) {
-    if (first[j] === second[i]) dp[i][j] = dp[i - 1][j - 1] + 1;
+    if (first[j - 1] === second[i - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
     else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
   }
 }
