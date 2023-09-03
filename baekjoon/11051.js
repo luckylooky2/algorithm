de1e53086029829af92add220afed1e7b8d83b96
luckylooky2 <luckylooky2@naver.com>
2023-09-03 18:36:22 +0900

    09.03 : dp(이항 계수 2)

---
+++ baekjoon/11051.js
@@ -0,0 +1,17 @@
+// 이항 계수 2 : 동적 계획법
+const [n, k] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((v) => parseInt(v, 10));
+
+const dp = new Array(n + 1).fill(null).map((v, i) => new Array(i + 1).fill(1));
+
+for (let i = 2; i <= n; i++) {
+  for (let j = 1; j < i; j++) {
+    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+  }
+}
+
+console.log(dp[n][k]);
