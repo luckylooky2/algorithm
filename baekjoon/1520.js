5724b93736fb180b27a2f5364ae537054c1710e9
luckylooky2 <luckylooky2@naver.com>
2023-09-06 23:58:32 +0900

    09.06 : dp + dfs(내리막 길)

--- baekjoon/1520.js
+++ baekjoon/1520.js
@@ -1,4 +1,4 @@
-// 내리막 길
+// 내리막 길 : 동적 계획법, 깊이 우선 탐색
 const map = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
@@ -6,43 +6,38 @@ const map = require("fs")
   .split("\n")
   .map((v) => v.split(" ").map((v) => parseInt(v, 10)));
 const [m, n] = map.shift();
-const visited = new Array(m).fill(null).map(() => new Array(n).fill(false));
-let answer = 0;
-let count = 0;
+const offset = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
+// 밑에서 지나온 길은 0으로 최신화되기 때문에
+// 처음에 0으로 초기화를 할 경우, 지나온 길과 갈 수 없는 길을 구분하지 못하게 됨
+// 단지 수 세기 문제처럼 -1로 초기화하여 해결
+// 0 : 충분히 지나갈 수 있는데, 도착 지점에 도달하지 못한 경우
+// -1 : 주변보다 값이 커서 절대로 지나갈 수 없는 경우
+const dp = new Array(m).fill(null).map(() => new Array(n).fill(-1));
+dp[m - 1][n - 1] = 1;
 
-function dfs(x, y, visited) {
-  if (x === n - 1 && y === m - 1) {
-    answer++;
-    return;
+function dfs(x, y) {
+  // 도착 지점을 1로 미리 지정해놓았기 때문에, 도착 지점에 도착할 수 있는 경우 1이 계속 전파될 수 있음
+  if (dp[x][y] !== -1) {
+    return dp[x][y];
   }
-  count++;
+  let count = 0;
 
-  if (x + 1 < n && map[y][x + 1] < map[y][x]) {
-    if (visited[y][x + 1]) return;
-    visited[y][x + 1] = true;
-    dfs(x + 1, y, visited);
-    visited[y][x + 1] = false;
+  for (let i = 0; i < 4; i++) {
+    const nx = x + offset[i][0];
+    const ny = y + offset[i][1];
+    if (nx >= 0 && nx < m && ny >= 0 && ny < n && map[x][y] > map[nx][ny]) {
+      count += dfs(nx, ny);
+      // console.log(dp); // dp 테이블의 변화
+    }
   }
-  if (y + 1 < m && map[y + 1][x] < map[y][x]) {
-    if (visited[y + 1][x]) return;
-    visited[y + 1][x] = true;
-    dfs(x, y + 1, visited);
-    visited[y + 1][x] = false;
-  }
-  if (x - 1 >= 0 && map[y][x - 1] < map[y][x]) {
-    if (visited[y][x - 1]) return;
-    visited[y][x - 1] = true;
-    dfs(x - 1, y, visited);
-    visited[y][x - 1] = false;
-  }
-  if (y - 1 >= 0 && map[y - 1][x] < map[y][x]) {
-    if (visited[y - 1][x]) return;
-    visited[y - 1][x] = true;
-    dfs(x, y - 1, visited);
-    visited[y - 1][x] = false;
-  }
+  // 마지막에 도착하여 1을 반환했을 때, dp 테이블에 저장하는 코드가 여기 있어야 네 군데를 다 돌아보고 결과 값을 저장할 수 있음
+  dp[x][y] = count;
+  return count;
 }
 
-dfs(0, 0, visited);
-console.log(answer);
-console.log(count);
+console.log(dfs(0, 0));
