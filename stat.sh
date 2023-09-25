20398850bd3511096f7fdae08b9db012a3c35691
luckylooky2 <luckylooky2@naver.com>
2023-09-26 00:19:22 +0900

    chore: add programmers directory

--- stat.sh
+++ stat.sh
@@ -2,17 +2,22 @@
 
 declare count_map
 
-js_num=$(find . -type f -name '*[0-9].js' | wc -l | tr -d ' ')
-cpp_num=$(find . -type f -name '*[0-9].cpp' | wc -l | tr -d ' ')
-c_num=$(find . -type f -name '*[0-9].c' | wc -l | tr -d ' ')
-py_num=$(find . -type f -name '*[0-9].py' | wc -l | tr -d ' ')
+js_b=$(find ./baekjoon -type f -name '*[0-9].js' | wc -l | tr -d ' ')
+cpp_b=$(find ./baekjoon -type f -name '*[0-9].cpp' | wc -l | tr -d ' ')
+c_b=$(find ./baekjoon -type f -name '*[0-9].c' | wc -l | tr -d ' ')
+py_b=$(find ./baekjoon -type f -name '*[0-9].py' | wc -l | tr -d ' ')
+js_p=$(find ./programmers -type f -name '*.js' | wc -l | tr -d ' ')
+cpp_p=$(find ./programmers -type f -name '*.cpp' | wc -l | tr -d ' ')
+c_p=$(find ./programmers -type f -name '*.c' | wc -l | tr -d ' ')
+py_p=$(find ./programmers -type f -name '*.py' | wc -l | tr -d ' ')
 
-count_map[${js_num}]="![](https://img.shields.io/badge/Javascript-F7DF1E?style=flat&logo=JavaScript&logoColor=white)"
-count_map[${cpp_num}]="![](https://img.shields.io/badge/C++-00599C?style=flat&logo=cplusplus&logoColor=white)"
-count_map[${c_num}]="![](https://img.shields.io/badge/C-A8B9CC?style=flat&logo=c&logoColor=white)"
-count_map[${py_num}]="![](https://img.shields.io/badge/Python-3776AB?style=flat&logo=python&logoColor=white)"
+array=($((js_b + js_p)) $((c_b + c_p)) $((cpp_b + cpp_p)) $((py_b + py_p)))
 
-array=(${js_num} ${c_num} ${cpp_num} ${py_num})
+count_map[$((js_b + js_p))]="![](https://img.shields.io/badge/Javascript-F7DF1E?style=flat&logo=JavaScript&logoColor=white)"
+count_map[$((c_b + c_p))]="![](https://img.shields.io/badge/C++-00599C?style=flat&logo=cplusplus&logoColor=white)"
+count_map[$((cpp_b + cpp_p))]="![](https://img.shields.io/badge/C-A8B9CC?style=flat&logo=c&logoColor=white)"
+count_map[$((py_b + py_p))]="![](https://img.shields.io/badge/Python-3776AB?style=flat&logo=python&logoColor=white)"
+
 sorted_array=($(for element in "${array[@]}"; do
   echo "$element"
 done | sort -r))
@@ -32,5 +37,3 @@ echo "
 |${count_map[sorted_array[3]]}|${sorted_array[3]}|
 |Total| ${total}|
 " > README.md
-
-# echo "통계 정보를 README.md 파일에 추가하세요." > README.md
