7d1cc3e55c941b9cfd79c747469d099a6d1831e9
luckylooky2 <luckylooky2@naver.com>
2023-09-28 11:10:59 +0900

    09.27 : stack, math(괄호의 값)

--- baekjoon/2504.js
+++ baekjoon/2504.js
@@ -1,5 +1,4 @@
 // 괄호의 값 : 스택
-
 const arr = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
@@ -8,40 +7,38 @@ const arr = require("fs")
 const stack = [];
 const result = [];
 let i = 0;
-const LP = "(",
-  RP = ")",
-  LB = "[",
-  RB = "]";
+let base = 1;
 let flag = false;
 
 while (i !== arr.length) {
   stack.push(arr[i]);
-  if (stack[stack.length - 1] === RP) {
+  const top = stack[stack.length - 1];
+  if (top === "(" || top === "[") base *= top === "(" ? 2 : 3;
+  if (top === ")" || top === "]") {
     const second = stack.pop();
     const first = stack.pop();
-    if (first === LP) {
-      result.push(2);
+    if (
+      (first === "(" && second === ")") ||
+      (first === "[" && second === "]")
+    ) {
+      if (
+        (!stack.length ||
+          stack[stack.length - 1] === "(" ||
+          stack[stack.length - 1] === "[") &&
+        !(arr[i - 1] === ")" || arr[i - 1] === "]")
+      )
+        result.push(base);
+      base /= first === "(" ? 2 : 3;
     } else {
       flag = true;
       break;
     }
-    if (arr[i + 1] === LP || arr[i + 1] === LB) result.push("+");
-    if (arr[i + 1] === RP || arr[i + 1] === RB) result.push("x");
-  } else if (stack[stack.length - 1] === RB) {
-    const second = stack.pop();
-    const first = stack.pop();
-    if (first === LB) {
-      result.push(3);
-    } else {
-      flag = true;
-      break;
-    }
-    if (arr[i + 1] === LP || arr[i + 1] === LB) result.push("+");
-    if (arr[i + 1] === RP || arr[i + 1] === RB) result.push("x");
   }
-  console.log(stack);
   i++;
 }
 
-// console.log(flag ? 0 : result);
-console.log(result);
+console.log(
+  flag || result.length === 0 || stack.length !== 0
+    ? 0
+    : result.reduce((a, b) => a + b)
+);
