fd6f2ddeffc109102b569d01a7f4f924057604f3
luckylooky2 <luckylooky2@naver.com>
2023-10-07 21:41:41 +0900

    10.07 : df, bfs(1로 만들기)

--- baekjoon/1463.js
+++ baekjoon/1463.js
@@ -1,8 +1,12 @@
-// 1로 만들기 : 동적 계획법
+// 1로 만들기 : 동적 계획법, 너비 우선 탐색
+const Queue = require("./queue");
+
 const n = parseInt(
   require("fs").readFileSync("/dev/stdin").toString().trim(),
   10
 );
+
+// dp ver.
 const dp = new Array(3).fill(null).map(() => new Array(n + 1).fill(0));
 
 // 첫 번째 row
@@ -37,3 +41,29 @@ for (let i = 2; i <= n; i++) {
 }
 
 console.log(dp[2][n]);
+
+// bfs ver.
+// const q = new Queue();
+// const dir = [1, 2, 3];
+// const map = new Array(n + 2).fill(0);
+// let flag = false;
+
+// if (n !== 1) q.push(1);
+// else return console.log(0);
+
+// map[1] = 0;
+// while (q.size !== 0) {
+//   const front = q.head.value;
+//   q.pop();
+//   for (let i = 0; i < dir.length; i++) {
+//     let next;
+//     if (i === 0) next = front + dir[i];
+//     else next = front * dir[i];
+//     if (next < 0 || next > n + 1 || map[next] > 0) continue;
+//     q.push(next);
+//     map[next] = map[front] + 1;
+//   }
+//   if (flag) break;
+// }
+
+// console.log(map[n]);
