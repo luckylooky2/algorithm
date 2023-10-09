b34129910d4d6e57052662caeaa441bc684fde3f
luckylooky2 <luckylooky2@naver.com>
2023-10-10 08:08:21 +0900

    chore: fix position due to duplicate problem

--- stat.sh
+++ stat.sh
@@ -11,29 +11,21 @@ cpp_p=$(find ./programmers -type f -name '*.cpp' | wc -l | tr -d ' ')
 c_p=$(find ./programmers -type f -name '*.c' | wc -l | tr -d ' ')
 py_p=$(find ./programmers -type f -name '*.py' | wc -l | tr -d ' ')
 
-array=($((js_b + js_p)) $((c_b + c_p)) $((cpp_b + cpp_p)) $((py_b + py_p)))
 
-count_map[$((js_b + js_p))]="![](https://img.shields.io/badge/Javascript-F7DF1E?style=flat&logo=JavaScript&logoColor=white)"
-count_map[$((cpp_b + cpp_p))]="![](https://img.shields.io/badge/C++-00599C?style=flat&logo=cplusplus&logoColor=white)"
-count_map[$((c_b + c_p))]="![](https://img.shields.io/badge/C-A8B9CC?style=flat&logo=c&logoColor=white)"
-count_map[$((py_b + py_p))]="![](https://img.shields.io/badge/Python-3776AB?style=flat&logo=python&logoColor=white)"
-
-sorted_array=($(for element in "${array[@]}"; do
-  echo "$element"
-done | sort -r))
+array=($((js_b + js_p)) $((cpp_b + cpp_p)) $((py_b + py_p)) $((c_b + c_p)))
 
 total=0
 
-for number in "${sorted_array[@]}"; do
+for number in "${array[@]}"; do
   total=$((total + number))
 done
 
 echo "
 | Language  | Number of solved problems |
 |:---|---:|
-|${count_map[sorted_array[0]]}|${sorted_array[0]}|
-|${count_map[sorted_array[1]]}|${sorted_array[1]}|
-|${count_map[sorted_array[2]]}|${sorted_array[2]}|
-|${count_map[sorted_array[3]]}|${sorted_array[3]}|
+|![](https://img.shields.io/badge/Javascript-F7DF1E?style=flat&logo=JavaScript&logoColor=white)|${array[0]}|
+|![](https://img.shields.io/badge/C++-00599C?style=flat&logo=cplusplus&logoColor=white)|${array[1]}|
+|![](https://img.shields.io/badge/Python-3776AB?style=flat&logo=python&logoColor=white)|${array[2]}|
+|![](https://img.shields.io/badge/C-A8B9CC?style=flat&logo=c&logoColor=white)|${array[3]}|
 |Total| ${total}|
-" > README.md
+"
