88a7d2667f7871a4d3706d2b18537a168de6afb9
luckylooky2 <luckylooky2@naver.com>
2023-11-11 23:58:11 +0900

    WIP : 수학(소수 찾기)

---
+++ baekjoon/1978.js
@@ -0,0 +1,23 @@
+// 소수 찾기
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.split(" ").map((v) => parseInt(v, 10)));
+const n = input.shift();
+const arr = input.shift();
+let answer = 0;
+
+arr.map((v) => {
+  let flag = false;
+  for (let i = 2; i * i < v; i++) {
+    if (v % i === 0) {
+      flag = true;
+      break;
+    }
+  }
+  if (flag) answer++;
+});
+
+console.log(answer);
