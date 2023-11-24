60002ba1fdb5a90b8f0b2a8f1e0897e706d23b88
luckylooky2 <luckylooky2@naver.com>
2023-11-25 00:33:08 +0900

    11.24 : hash(회사에 있는 사람)

--- baekjoon/7785.js
+++ baekjoon/7785.js
@@ -1,7 +1,24 @@
 // 회사에 있는 사람
-const input = require("fs")
+const arr = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
   .split("\n")
   .map((v) => v.split(" "));
+const [n] = arr.shift();
+const log = {};
+
+for ([name, event] of arr) {
+  if (event === "enter") log[name] = true;
+  else log[name] = false;
+}
+
+// string 정렬은 콜백 함수가 필요하지 않음
+console.log(
+  Object.entries(log)
+    .filter(([key, value]) => value === true)
+    .map((v) => v[0])
+    .sort()
+    .reverse()
+    .join("\n")
+);
