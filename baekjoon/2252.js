a7eed512b3bb522e7a6339eca816ec0b0caa5bdf
luckylooky2 <luckylooky2@naver.com>
2023-12-10 17:29:16 +0900

    12.10 : topological sort(줄 세우기)

---
+++ baekjoon/2252.js
@@ -0,0 +1,47 @@
+// 줄 세우기 : 위상 정렬
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.split(" ").map((v) => parseInt(v, 10)));
+const [n, m] = input.shift();
+const arr = input;
+const graph = {};
+
+for (let [first, second] of arr) {
+  if (!graph[first]) graph[first] = [second];
+  else graph[first].push(second);
+}
+
+function topologicalSort(graph) {
+  const indegrees = new Array(n + 1).fill(0);
+  const q = [];
+  const answer = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (graph[i]) {
+      for (let value of graph[i]) {
+        indegrees[value]++;
+      }
+    }
+  }
+
+  for (let i = 1; i <= n; i++) if (indegrees[i] === 0) q.push(i);
+
+  while (q.length !== 0) {
+    const top = q.shift();
+    answer.push(top);
+    if (!graph[top]) continue;
+    for (let node of graph[top]) {
+      indegrees[node]--;
+      if (!indegrees[node]) {
+        q.push(node);
+      }
+    }
+  }
+
+  console.log(answer.join(" "));
+}
+
+topologicalSort(graph);
