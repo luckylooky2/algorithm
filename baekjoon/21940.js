f1fd31aa8fd9291ad5355645948f554d26a7fdd1
luckylooky2 <luckylooky2@naver.com>
2024-01-04 22:33:37 +0900

    comment : change variable names(가운데에서 만나요)

--- baekjoon/21940.js
+++ baekjoon/21940.js
@@ -9,12 +9,13 @@ const [cityCnt, pathCnt] = input.shift();
 const paths = new Array(pathCnt);
 const [personCnt] = input[pathCnt];
 const liveCity = input[pathCnt + 1];
-for (let i = 0; i < pathCnt; i++) {
-  paths[i] = input[i];
-}
 const distance = new Array(cityCnt)
   .fill(null)
   .map(() => new Array(cityCnt).fill(Infinity));
+
+for (let i = 0; i < pathCnt; i++) {
+  paths[i] = input[i];
+}
 for (let i = 0; i < cityCnt; i++) {
   distance[i][i] = 0;
 }
@@ -37,28 +38,28 @@ for (let i = 0; i < cityCnt; i++) {
   }
 }
 
-const result = [];
+const longestRoundTrip = [];
 const answer = [];
 
 // 브루트 포스
 for (let i = 0; i < cityCnt; i++) {
-  const total = [];
+  const roundTrip = [];
   for (let city of liveCity) {
     city--;
-    if (city === i) total.push(0);
+    if (city === i) roundTrip.push(0);
     else {
-      total.push(distance[city][i] + distance[i][city]);
+      roundTrip.push(distance[city][i] + distance[i][city]);
     }
   }
-  const sorted = total.sort((a, b) => a - b);
-  result.push([sorted[sorted.length - 1], i + 1]);
+  const sortedRoundTrip = roundTrip.sort((a, b) => a - b);
+  longestRoundTrip.push([sortedRoundTrip[sortedRoundTrip.length - 1], i + 1]);
 }
 
-const sorted = result.sort((a, b) => a[0] - b[0]);
-const smallest = sorted[0][0];
+const sortedLongestRoundTrip = longestRoundTrip.sort((a, b) => a[0] - b[0]);
+const smallestRoundTrip = sortedLongestRoundTrip[0][0];
 
-for (let elem of sorted) {
-  if (smallest === elem[0]) {
+for (let elem of sortedLongestRoundTrip) {
+  if (smallestRoundTrip === elem[0]) {
     answer.push(elem[1]);
   } else break;
 }
