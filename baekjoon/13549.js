0f3e9774a5c1e7386fea21f97714f06ecf436818
luckylooky2 <luckylooky2@naver.com>
2024-01-16 23:49:44 +0900

    comment : Array satisfies the time complexity(숨바꼭질 3)

--- baekjoon/13549.js
+++ baekjoon/13549.js
@@ -10,75 +10,21 @@ const map = new Array(200001).fill(0);
 let count = 1;
 let dir = [1, -1];
 
-class Node {
-  constructor(v = 0) {
-    this.value = v;
-    this.prev = null;
-    this.next = null;
-  }
-}
-
-class Queue {
-  constructor(n) {
-    let prevNode = null;
-
-    this.head = null;
-    this.tail = null;
-    this.size = 0;
-
-    for (let i = 0; i < n; i++) {
-      const newNode = new Node(i + 1);
-      if (this.head === null) {
-        this.head = newNode;
-        this.tail = newNode;
-      } else {
-        prevNode.next = newNode;
-        newNode.prev = prevNode;
-        this.tail = newNode;
-      }
-      prevNode = newNode;
-      this.size++;
-    }
-  }
-
-  pop = () => {
-    const nextNode = this.head.next;
-
-    if (nextNode !== null) nextNode.prev = null;
-    else this.tail = null;
-    this.head = nextNode;
-    this.size--;
-  };
-
-  push = (value) => {
-    const newNode = new Node(value);
-    const lastNode = this.tail;
-
-    if (this.tail !== null) lastNode.next = newNode;
-    else this.head = newNode;
-    newNode.prev = lastNode;
-    this.tail = newNode;
-    this.size++;
-  };
-
-  front = () => this.head.value;
-}
-
-let q = new Queue();
+let q = [];
 
 if (n <= k) {
   q.push([k, count]);
   map[k] = count;
 }
 
-while (q.size !== 0) {
-  const [prev, count] = q.front();
-  q.pop();
+while (q.length !== 0) {
+  const [prev, count] = q.shift();
 
   if (prev === n) {
     console.log(count - 1);
     break;
   }
+
   const mod = prev % 2;
   if (mod === 0) {
     const quotinent = prev / 2;
