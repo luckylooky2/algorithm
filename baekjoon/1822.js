068898da1acbc0400e2ae46da72e9b8ab7551bfe
luckylooky2 <luckylooky2@naver.com>
2024-02-01 10:40:50 +0900

    2.1 : binary search(차집합)

--- baekjoon/1822.js
+++ baekjoon/1822.js
@@ -9,6 +9,8 @@ const [] = input.shift();
 const sortFunction = (a, b) => a - b;
 const setA = input.shift().sort(sortFunction);
 const setB = input.shift().sort(sortFunction);
+
+// ver 1.
 let indexA = 0;
 let indexB = 0;
 const answer = [];
@@ -37,4 +39,32 @@ console.log(answer.length === 0 ? 0 : `${answer.length}\n${answer.join(" ")}`);
 
 // 입력값 출력 실수
 
-// 이분 탐색으로 풀어보기
+// 이분 탐색으로 풀어보기 => 이분 탐색으로 A의 원소가 B에 있는지 확인
+// 있다면? 제외, 없다면? 답에 포함
+
+// ver 2.
+// const answer = [];
+
+// function binarySearch(start, end, target, set) {
+//   if (start === end) {
+//     return set[start] === target ? null : set[start];
+//   }
+
+//   const mid = Math.floor((start + end) / 2);
+
+//   if (set[mid] < target) {
+//     return binarySearch(mid + 1, end, target, set);
+//   } else {
+//     return binarySearch(start, mid, target, set);
+//   }
+// }
+
+// for (let i = 0; i < setA.length; i++) {
+//   const curr = setA[i];
+//   const res = binarySearch(0, setB.length - 1, curr, setB);
+//   if (res !== null) {
+//     answer.push(curr);
+//   }
+// }
+
+// console.log(answer.length === 0 ? 0 : `${answer.length}\n${answer.join(" ")}`);
