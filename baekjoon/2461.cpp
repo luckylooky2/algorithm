9d83894d488f5b636f484603dc5b3882c2dfeffe
luckylooky2 <luckylooky2@naver.com>
2024-03-03 16:06:12 +0900

    Comment : 대표 선수

--- baekjoon/2461.cpp
+++ baekjoon/2461.cpp
@@ -1,43 +1,57 @@
 // 대표 선수 : 투 포인터, 정렬
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <math.h>
 
 using namespace std;
-#define X first
-#define Y second
-#define ll long long
-int n, m;
-int arr[1005][1005];
-int ans = 0x7f7f7f7f;
-vector<int> idx; //각 반에서 현재 단계의 인덱스를 저장하는 벡터
+
 int main(void) {
+	int n, m;
+	vector<vector<int> > classes;
+	vector<int> indexes;
+	int			answer = 0x7f7f7f7f;
+
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n >> m;
-    for (int i = 0;i < n;i++) {
-        idx.push_back(0);
-        for (int j = 0;j < m;j++) cin >> arr[i][j];
+
+    for (int i = 0; i < n; i++) {
+		vector<int>	classVec;
+		int			tmp;
+        indexes.push_back(0);
+        for (int j = 0; j < m; j++) {
+			cin >> tmp;
+			classVec.push_back(tmp);
+		}
+		classes.push_back(classVec);
     }
-    //과정 1
-    for (int i = 0;i < n;i++) sort(arr[i], arr[i] + m);
-    while (1) {
-        int mntm;
-        int mx = -1;
-        int mn = 0x7f7f7f7f;
-        //과정 2
+    
+    for (int i = 0; i < n; i++) {
+		sort(classes[i].begin(), classes[i].end());
+	}
+
+    while (true) {
+        int minIndex;
+        int max = -1;
+        int min = 0x7f7f7f7f;
+        
         for (int i = 0;i < n;i++) {
-            if (mn > arr[i][idx[i]]) {
-                mn = arr[i][idx[i]];
-                mntm = i;
+            if (min > classes[i][indexes[i]]) {
+                min = classes[i][indexes[i]];
+                minIndex = i;
             }
-            if (mx < arr[i][idx[i]])  mx = arr[i][idx[i]];
+            if (max < classes[i][indexes[i]]) {
+				max = classes[i][indexes[i]];
+			}
         }
-        //과정 3
-        ans = min(ans, mx-mn);
-        //과정 4
-        idx[mntm] += 1;
-        //과정 5
-        if (idx[mntm] == m) break;
+		if (answer > max - min) {
+        	answer = max - min;
+		}
+        indexes[minIndex] += 1;
+        if (indexes[minIndex] == m) {
+			break;
+		}
     }
-    cout << ans;
+    cout << answer;
 }
