0681d649bbcfa69f3030d59194a21b5f7ea0a05e
luckylooky2 <luckylooky2@naver.com>
2024-03-21 10:37:19 +0900

    3.21: MST(네트워크 연결)

---
+++ baekjoon/1922.js
@@ -0,0 +1,36 @@
+// 네트워크 연결 : 최소 스패닝 트리, 그래프
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.split(" ").map((v) => Number(v)));
+const [n] = input.shift();
+const [m] = input.shift();
+const costs = input.slice().sort((a, b) => a[2] - b[2]);
+let answer = 0;
+const parents = new Array(n + 1).fill(null).map((_v, i) => i);
+const getParent = function (x) {
+  return x === parents[x] ? parents[x] : getParent(parents[x]);
+};
+const unionParent = function (x, y) {
+  x = getParent(x);
+  y = getParent(y);
+  if (x < y) {
+    parents[y] = x;
+  } else {
+    parents[x] = y;
+  }
+};
+
+for (let [start, end, cost] of costs) {
+  const x = getParent(start);
+  const y = getParent(end);
+  // a === b인 경우 union 하지 않는다
+  if (x !== y) {
+    unionParent(x, y);
+    answer += cost;
+  }
+}
+
+console.log(answer);
