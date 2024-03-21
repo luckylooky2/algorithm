80da4a48f3cb18d8b3f60ec6abafdcefa711ee7f
luckylooky2 <luckylooky2@naver.com>
2024-03-21 18:24:41 +0900

    Chore: update stat.sh problems count by platform

--- stat.sh
+++ stat.sh
@@ -14,21 +14,28 @@ py_p=$(find ./programmers -type f -name '*.py' | wc -l | tr -d ' ')
 duplicate_b=$(find ./baekjoon -type f -name '*[0-9].*' -exec basename {} \; | sed 's/\..*$//' | sort | uniq -d | wc -l)
 duplicate_p=$(find ./programmers -type f -name '*' -exec basename {} \; | sed 's/\..*$//' | sort | uniq -d | wc -l)
 
-array=($((js_b + js_p)) $((cpp_b + cpp_p)) $((py_b + py_p)) $((c_b + c_p)))
+arrayByLanguage=($((js_b + js_p)) $((cpp_b + cpp_p)) $((py_b + py_p)) $((c_b + c_p)))
+arrayByPlatform=($((js_b + cpp_b + c_b + py_b - duplicate_b)) $((js_p + cpp_p + c_p + py_p - duplicate_p)))
 
 total=0
 duplicate=$((duplicate_b + duplicate_p))
 
-for number in "${array[@]}"; do
+for number in "${arrayByLanguage[@]}"; do
   total=$((total + number))
 done
 
 echo "
 | Language  | Number of solved problems |
 |:---|---:|
-|![](https://img.shields.io/badge/Javascript-F7DF1E?style=flat&logo=JavaScript&logoColor=white)|${array[0]}|
-|![](https://img.shields.io/badge/C++-00599C?style=flat&logo=cplusplus&logoColor=white)|${array[1]}|
-|![](https://img.shields.io/badge/Python-3776AB?style=flat&logo=python&logoColor=white)|${array[2]}|
-|![](https://img.shields.io/badge/C-A8B9CC?style=flat&logo=c&logoColor=white)|${array[3]}|
+|![](https://img.shields.io/badge/Javascript-F7DF1E?style=flat&logo=JavaScript&logoColor=white)|${arrayByLanguage[0]}|
+|![](https://img.shields.io/badge/C++-00599C?style=flat&logo=cplusplus&logoColor=white)|${arrayByLanguage[1]}|
+|![](https://img.shields.io/badge/Python-3776AB?style=flat&logo=python&logoColor=white)|${arrayByLanguage[2]}|
+|![](https://img.shields.io/badge/C-A8B9CC?style=flat&logo=c&logoColor=white)|${arrayByLanguage[3]}|
+|Total| $((total - duplicate))|
+
+| Platform  | Number of solved problems |
+|:---|---:|
+|![baekjoon]("https://github.com/luckylooky2/algorithm/assets/85822311/4d8a8f6e-4414-472b-8f7c-d3020fc322df")|${arrayByPlatform[0]}|
+|![programmers]("https://github.com/luckylooky2/algorithm/assets/85822311/9956c5c1-e76d-4fc1-aa9e-3177b9451db0")|${arrayByPlatform[1]}|
 |Total| $((total - duplicate))|
 " > README.md
