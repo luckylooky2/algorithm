1736287cfff24dcb054473a6df25104ccac37de5
luckylooky2 <luckylooky2@naver.com>
2024-08-04 23:58:46 +0900

    엄청난 부자2

---
+++ baekjoon/1271.js
@@ -0,0 +1,15 @@
+// 엄청난 부자2
+const [n, m] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((v) => BigInt(v));
+
+function solve(n, m) {
+  const quotinent = n / m;
+  const remainder = n % m;
+  return [String(quotinent), String(remainder)];
+}
+
+console.log(solve(n, m).join("\n"));
